3de76758f6d866cb6049aae0f8ce448ba49f03f0
h662Olbm <h662@olbm.app>
2023-06-11 22:36:20 +0900

    story

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,32 @@
+"use client";
+
 import Image from "next/image";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 import TeamCard from "@/components/TeamCard";
 
 const Home = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+  };
+
   return (
     <div className="max-w-screen-md mx-auto shadow-lg min-h-screen text-black font-bold font-kcc">
       <header className="px-4 py-2 flex justify-between items-center w-full h-fit shadow-sm">
-        <div className="text-2xl">가 보 자 고 !</div>
-        <div className="">
+        <div className="text-xl">가 보 자 고 !</div>
+        <div className="text-sm">
           <button className=" text-purple-500 hover:text-purple-300">
             가보자고?
           </button>
-          <button className="ml-4 text-purple-500 hover:text-purple-300">
+          <button className="ml-2 text-purple-500 hover:text-purple-300">
             스토리
           </button>
-          <button className="ml-4 text-purple-500 hover:text-purple-300">
+          <button className="ml-2 text-purple-500 hover:text-purple-300">
             로드맵
           </button>
-          <button className="ml-4 text-purple-500 hover:text-purple-300">
+          <button className="ml-2 text-purple-500 hover:text-purple-300">
             팀
           </button>
         </div>
@@ -24,15 +34,15 @@ const Home = () => {
 
       <main className="px-2">
         <div className="flex flex-col items-center mt-8">
-          <div className="w-full">
+          <div className="w-full text-sm">
             WEB3 프로젝트 &apos;가보자고&apos;는
             <br /> 로컬 맛집 활성화를 위해 탄생하였습니다.
           </div>
           <div className="mt-4">
             <Image
               src="/images/restaurant.png"
-              width={160}
-              height={160}
+              width={120}
+              height={120}
               alt="restaurant"
             />
           </div>
@@ -40,15 +50,15 @@ const Home = () => {
             <Image
               className="rounded-full"
               src="/images/1.png"
-              width={160}
-              height={160}
+              width={120}
+              height={120}
               alt="Holder"
             />
-            <div className="w-40 h-40 bg-purple-200 rounded-full flex justify-center items-center text-2xl text-gray-700">
+            <div className="w-32 h-32 bg-purple-200 rounded-full flex justify-center items-center text-2xl text-gray-700">
               가 보 자 고
             </div>
           </div>
-          <div className="w-full mt-4 text-right">
+          <div className="w-full mt-4 text-right text-sm">
             NFT 홀더들에게 지역기반의 숨은 찐 맛집을 소개하고,
             <br />
             식당은 Web3 커뮤니티 기반으로 마케팅 활동을 할 수
@@ -56,6 +66,42 @@ const Home = () => {
         </div>
         <div>
           <div className="text-2xl mb-4">스토리</div>
+          <Carousel>
+            <div>
+              <Image
+                className="rounded-full"
+                src="/images/1.png"
+                width={120}
+                height={120}
+                alt="Holder"
+              />
+              <p className="legend">
+                옛날 옛날에 배고픈 토끼가 살고 있었습니다.
+              </p>
+            </div>
+            <div>
+              <Image
+                className="rounded-full"
+                src="/images/1.png"
+                width={120}
+                height={120}
+                alt="Holder"
+              />
+              <p className="legend">
+                토끼는 지역 로컬 찐 맛집 식당을 찾아냈습니다.
+              </p>
+            </div>
+            <div>
+              <Image
+                className="rounded-full"
+                src="/images/1.png"
+                width={120}
+                height={120}
+                alt="Holder"
+              />
+              <p className="legend">배고픈 토끼는 배부른 토끼가 되었답니다!</p>
+            </div>
+          </Carousel>
         </div>
         <div>
           <div className="text-2xl mb-4">로드맵</div>
